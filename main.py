33a0783369fee70a82df8bf9a72302cfb2e2b2d8
alihashe <alihashe@iu.edu>
2022-05-25 21:16:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-import requests as req
+from msilib.schema import Class
+from typing_extensions import Self
+from unicodedata import name
+import requests
 import pandas as pd
 
+
+class Node:
+    """This defines each location on the map."""
+    def __init__(self, name="nihil", location=(-1, -1)):
+        self.name = name
+        self.location = location
+
+    def returnName(self):
+        return self.name
+
+    def returnLocation(self):
+        return self.location
+
+
+  
+
+#39.164737530481396, -86.5129661849151     Forest Dorm
